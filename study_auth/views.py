56d36b12373ede06fb5b3d73efff3b83b6c44cb5
Ippei523 <ippei523@gmail.com>
2024-02-10 00:36:08 +0900

    viewファイルの変更

--- study_auth/views.py
+++ study_auth/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from .models import User
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
 # Create your views here.
+
+@api_view(['POST'])
+def register(request):
+    email = request.data.get('email')
+    password = request.data.get('password')
+    user = User.objects.create(email=email, password=password)
+    return Response({'id': user.id, 'email': user.email, 'created_at': user.created_at, 'updated_at': user.updated_at,})
