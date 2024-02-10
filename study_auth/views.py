3f0680c6188edfa5bc9f3aa1bc6e065580953bbb
Ippei523 <ippei523@gmail.com>
2024-02-10 12:14:00 +0900

    viewファイル追記

--- study_auth/views.py
+++ study_auth/views.py
@@ -1,12 +1,19 @@
 from .models import User
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.views import APIView
+from django.contrib.auth.hashers import make_password
 
-# Create your views here.
+class UsersView(APIView):
+    def post(self, request, *args, **kwargs):
+        user = User.objects.create(
+            email=request.data['email'],
+            password=make_password(request.data['password'])
+        )
+        return Response({'id': user.id})
 
-@api_view(['POST'])
-def register(request):
-    email = request.data.get('email')
-    password = request.data.get('password')
-    user = User.objects.create(email=email, password=password)
-    return Response({'id': user.id, 'email': user.email, 'created_at': user.created_at, 'updated_at': user.updated_at,})
+    def get(self, request, *args, **kwargs):
+        users = User.objects.all()
+        return Response([{'id': user.id, 'email': user.email} for user in users])
